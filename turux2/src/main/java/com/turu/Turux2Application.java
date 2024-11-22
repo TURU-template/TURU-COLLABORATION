60f589d2bd58a45ae309f21e723a07de409db8f5
Muhammad Iqbal Rasyid (Bal Gans Ea) <59911597+Otachiking@users.noreply.github.com>
2024-11-22 16:09:05 +0700

    PROJECT BERJAMAAH versi 2
    UInya udah ada Login + Register, Beranda, Lagu/Radio, Akun. Login masih lom work tapi

---
+++ turux2/src/main/java/com/turu/Turux2Application.java
@@ -0,0 +1,16 @@
+package com.turu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.turu")
+
+public class Turux2Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Turux2Application.class, args);
+		System.out.println("WOIIII Iqbal Rasyid Gantengsadsd");
+	}
+
+}
---
+++ turux2/src/test/java/com/iqbal/Turux1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iqbal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Turux1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
