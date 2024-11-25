c22cab080a2a55b0b5f9255c0aa281d86db1abf1
Dev-TheAce <m.tiroyhidayat13@gmail.com>
2024-11-26 00:21:55 +0700

    Edit salah nama class

--- turux2/src/test/java/com/iqbal/Turux1ApplicationTests.java
+++ turux2/src/test/java/com/iqbal/Turux1ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Turux1ApplicationTests {
+class Turux2ApplicationTests {
 
 	@Test
 	void contextLoads() {
