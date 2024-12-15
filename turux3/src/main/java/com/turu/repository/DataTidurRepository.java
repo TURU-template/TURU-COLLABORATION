4f8d0a40157c6cd8ab3852fd23dc92d3f893363f
Aisya Safiya <safiyaaisya889@gmail.com>
2024-12-15 11:22:34 +0700

    Update DataTidurRepository.java
    Update DataTidurRepository biar ambil data rentang 7 hari

--- turux3/src/main/java/com/turu/repository/DataTidurRepository.java
+++ turux3/src/main/java/com/turu/repository/DataTidurRepository.java
@@ -1,10 +1,11 @@
 package com.turu.repository;
 
+import java.time.LocalDate;
 import com.turu.model.DataTidur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface DataTidurRepository extends JpaRepository<DataTidur, Long> {
-    List<DataTidur> findByPenggunaId(Long penggunaId);
+    List<DataTidur> findByPenggunaIdAndTanggalBetween(Long penggunaId, LocalDate startDate, LocalDate endDate);
 }
