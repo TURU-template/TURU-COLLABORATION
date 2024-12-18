6515ebdb73e2766467d780127b2c4773539b53e3
Aisya Safiya <safiyaaisya889@gmail.com>
2024-12-18 14:33:38 +0700

    Update DataTidurService.java

--- turux3/src/main/java/com/turu/service/DataTidurService.java
+++ turux3/src/main/java/com/turu/service/DataTidurService.java
@@ -1,5 +1,16 @@
-package com.turu.service;
+import com.turu.model.DataTidur;
+import com.turu.repository.DataTidurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+import java.util.List;
 
+@Service
 public class DataTidurService {
+    @Autowired
+    private DataTidurRepository dataTidurRepository;
 
+    public List<DataTidur> getDataTidurMingguan(LocalDate startDate, LocalDate endDate) {
+        return dataTidurRepository.findByTanggalBetween(startDate, endDate);
+    }
 }
